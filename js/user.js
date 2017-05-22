3aeb0ec864a9ebb888d7e5d6fd9c9efde122d9c3
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-05-22 15:25:11 -0700

    add use strict to all js files, begin user object

---
+++ js/user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var user = {
+    name: '',
+    color: '',
+    mood: '',
+    particle: '',
+    marsupial: '',
+    fontSize: 0,
+    pFont: 0,
+    margin: 0,
+    float: '',
+
+    color1: '',
+    color2: '',
+    color3: '',
+
+    //methods
+    
+
+    //input for each property()
+    //localStorage set()
+};
