a045f78c160eff5b5922dd698a5f20b5c4398432
Meryl Turner <merylturner08@gmail.com>
2017-05-22 15:43:49 -0700

    relinked css and js files to welcome.html

--- js/narrator.js
+++ js/narrator.js
@@ -4,24 +4,27 @@ function Narrator() {
     this.self = document.getElementById('narrator');
     this.hideMe = function () {
         this.self.setAttribute('class', 'hideMe');
-    },
-        this.showMe = function () {
-            this.self.setAttribute('class', 'showMe');
-        }
+    };
+    this.showMe = function () {
+        this.self.setAttribute('class', 'showMe');
+    };
     this.submit = function () {
         user.saveInfo();
+        user.storeInfo();
         window.location('#')
-    },
-        this.getInfo = function () {
-            User = JSON.parse(localStorage.getItem('user'));
-        },
+    };
+    this.getInfo = function () {
+        User = JSON.parse(localStorage.getItem('user'));
+    };
 }
+
 var button = document.getElementById('closeout');
 button.addEventListener('click', hideMeHandler);
 function hideMeHandler() {
     event.preventDefault();
     Narrator.hideMe();
 }
+
 var showTab = document.getElementsByClassName('hideMe');
 showTab.addEventListener('hover', showTabHandler);
 function showTabHandler() {
