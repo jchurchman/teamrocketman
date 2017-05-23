ad7f052a79493a0669f7117d2bc02215e69ec17a
Joe Churchman <churchman.joe@gmail.com>
2017-05-23 11:35:19 -0700

    roughed in above-the-fold load work

--- js/layout.js
+++ js/layout.js
@@ -2,3 +2,13 @@
 
 var layoutNarrator = new Narrator();
 
+layoutNarrator.guest = JSON.parse(localStorage.getItem('userInfo'));
+
+var subHeader = document.getElementsByTagName('h2')[0];
+var critterName =  layoutNarrator.guest.marsupial;
+var userName = layoutNarrator.guest.name;
+subHeader.innerText = 'This is a Webpage about ' + critterName + ' by ' + userName + '.';
+
+var navBar = document.getElementsByTagName('ul')[0];
+var navContent = '<li>' + layoutNarrator.guest.color + '</li><li>' + layoutNarrator.guest.mood + '</li><li>' + layoutNarrator.guest.particle + '</li>';
+navBar.innerHTML = navContent;
