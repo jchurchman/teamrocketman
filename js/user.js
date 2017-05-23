f931da82f437e7ba11722e9160b0604bedc2a8f9
Meryl Turner <merylturner08@gmail.com>
2017-05-22 17:41:49 -0700

    moved userSubmissions method to narrator object

--- js/user.js
+++ js/user.js
@@ -16,14 +16,6 @@ var user = {
     color2: '',
     color3: '',
 
-    userSubmission: function() {
-        this.name = document.getElementById('nameInput').value;
-        this.color = document.getElementById('colorInput').value;
-        this.mood = document.getElementById('moodInput').value;
-        this.particle = document.getElementById('particleInput').value;
-        this.marsupial = document.querySelector('input[name = "marsupial"]:checked').value;
-    },
-
     locallyStoreUser: function() {
         var userArrayStringified = JSON.stringify(this);
         localStorage.setItem('userArray', userArrayStringified);
--- js/welcome.js
+++ js/welcome.js
@@ -26,9 +26,16 @@ var welcomeNarrator = new Narrator();
 var submit = document.getElementById('submit');
 submit.addEventListener('click', submissionHandler);
 function submissionHandler() {
-	event.preventDefault();
-	user.userSubmission();
-	user.locallyStoreUser();
+    event.preventDefault();
+    welcomeNarrator.userSubmission();
+    user.locallyStoreUser();
 }
 
 //add userSubmissions as a prototype function of welcomeNarrator
+Narrator.prototype.userSubmission = function () {
+    user.name = document.getElementById('nameInput').value;
+    user.color = document.getElementById('colorInput').value;
+    user.mood = document.getElementById('moodInput').value;
+    user.particle = document.getElementById('particleInput').value;
+    user.marsupial = document.querySelector('input[name = "marsupial"]:checked').value;
+};
