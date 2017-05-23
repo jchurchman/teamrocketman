d188510e33dab405806c98aad9bdce3ecdbf9a88
Joe Churchman <churchman.joe@gmail.com>
2017-05-23 11:49:05 -0700

    more rough in page load from userInfo TODO: marsupials

--- js/layout.js
+++ js/layout.js
@@ -12,3 +12,15 @@ subHeader.innerText = 'This is a Webpage about ' + critterName + ' by ' + userNa
 var navBar = document.getElementsByTagName('ul')[0];
 var navContent = '<li>' + layoutNarrator.guest.color + '</li><li>' + layoutNarrator.guest.mood + '</li><li>' + layoutNarrator.guest.particle + '</li>';
 navBar.innerHTML = navContent;
+
+var articleHeadingLoc = document.getElementsByTagName('h3')[0];
+var articleHeading = layoutNarrator.guest.marsupial.name;
+articleHeadingLoc.innerText = articleHeading;
+
+var critterBlurbLoc = document.getElementsByTagName('p')[0];
+var critterBlurb = layoutNarrator.guest.marsupial.critterBlurb;
+critterBlurbLoc.innerHTML = '<img src=\'\'/>' + critterBlurb;
+
+var critterPic = document.getElementsByTagName('img')[0];
+var critterSrc = layoutNarrator.guest.marsupial.filepath;
+critterPic.src = critterSrc;
