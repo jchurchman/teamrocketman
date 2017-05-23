fbdaaa7bfd02dfcfe9b2edb8a518b13c556d4ff3
Joe Churchman <churchman.joe@gmail.com>
2017-05-23 12:03:08 -0700

    Moved content pop functions to narrator.js methods

--- js/layout.js
+++ js/layout.js
@@ -2,25 +2,34 @@
 
 var layoutNarrator = new Narrator();
 
-layoutNarrator.guest = JSON.parse(localStorage.getItem('userInfo'));
+layoutNarrator.getGuest();
+layoutNarrator.popContent = function () {
+    this.popSubHeader();
+    this.popNavBar();
+    this.popArticleHeading();
+    this.popCritterBlurb();
+    this.popCritterPic();
+};
+layoutNarrator.popContent();
+// layoutNarrator.guest = JSON.parse(localStorage.getItem('userInfo'));
 
-var subHeader = document.getElementsByTagName('h2')[0];
-var critterName =  layoutNarrator.guest.marsupial;
-var userName = layoutNarrator.guest.name;
-subHeader.innerText = 'This is a Webpage about ' + critterName + ' by ' + userName + '.';
+// var subHeader = document.getElementsByTagName('h2')[0];
+// var critterName =  layoutNarrator.guest.marsupial.name;
+// var userName = layoutNarrator.guest.name;
+// subHeader.innerText = 'This is a Webpage about ' + critterName + ' by ' + userName + '.';
 
-var navBar = document.getElementsByTagName('ul')[0];
-var navContent = '<li>' + layoutNarrator.guest.color + '</li><li>' + layoutNarrator.guest.mood + '</li><li>' + layoutNarrator.guest.particle + '</li>';
-navBar.innerHTML = navContent;
+// var navBar = document.getElementsByTagName('ul')[0];
+// var navContent = '<li>' + layoutNarrator.guest.color + '</li><li>' + layoutNarrator.guest.mood + '</li><li>' + layoutNarrator.guest.particle + '</li>';
+// navBar.innerHTML = navContent;
 
-var articleHeadingLoc = document.getElementsByTagName('h3')[0];
-var articleHeading = layoutNarrator.guest.marsupial.name;
-articleHeadingLoc.innerText = articleHeading;
+// var articleHeadingLoc = document.getElementsByTagName('h3')[0];
+// var articleHeading = layoutNarrator.guest.marsupial.name;
+// articleHeadingLoc.innerText = articleHeading;
 
-var critterBlurbLoc = document.getElementsByTagName('p')[0];
-var critterBlurb = layoutNarrator.guest.marsupial.critterBlurb;
-critterBlurbLoc.innerHTML = '<img src=\'\'/>' + critterBlurb;
+// var critterBlurbLoc = document.getElementsByTagName('p')[0];
+// var critterBlurb = layoutNarrator.guest.marsupial.critterBlurb;
+// critterBlurbLoc.innerHTML = '<img src=\'\'/>' + critterBlurb;
 
-var critterPic = document.getElementsByTagName('img')[0];
-var critterSrc = layoutNarrator.guest.marsupial.filepath;
-critterPic.src = critterSrc;
+// var critterPic = document.getElementsByTagName('img')[0];
+// var critterSrc = layoutNarrator.guest.marsupial.filepath;
+// critterPic.src = critterSrc;
