04c3ed7a6ce052225f2e36f91f313aa86cdb4992
Meryl Turner <merylturner08@gmail.com>
2017-05-24 15:41:30 -0700

    creating phases for each page

--- js/color.js
+++ js/color.js
@@ -1,87 +1,86 @@
 'use strict';
 
-var colorNarrator = new Narrator ();
+var colorNarrator = new Narrator();
 var colorUser = new User();
+var currentPhase = 1;
 
-var colorPhaseOne = new Phase (
+var colorPhaseOne = new Phase(
     'Welcome to a world of color! Black and white can be very appealing, but color can also add a lot to a page. Let\'s experiment! First, choose a color for the background of your page.',
     '<input type="submit" value="Onward!">',
-    '<input type="radio" value="grey">  Grey', 
-    '<input type="radio" value="lightblue">  Light Blue' 
+    '<input type="radio" name ="colorId" value="grey">  Grey',
+    '<input type="radio" name ="colorId" value="lightblue">  Light Blue'
 );
 
-var colorPhaseTwo = new Phase (
+var colorPhaseTwo = new Phase(
     'Next, choose a color for your header.',
     '<input type="submit" value="Onward!">',
-    '<input type="radio" value="darkgreen">  Dark Green', 
-    '<input type="radio" value="maroon">  Maroon' 
+    '<input type="radio"  name ="colorId"value="darkgreen">  Dark Green',
+    '<input type="radio" name ="colorId" value="maroon">  Maroon'
 );
 
-var colorPhaseThree = new Phase (
+var colorPhaseThree = new Phase(
     'Lastly, choose a color for the text in your paragraph.',
     '<input type="submit" value="Onward!">',
-    '<input type="radio" value="darkblue">  Dark Blue', 
-    '<input type="radio" value="red">  Red'
+    '<input type="radio" name ="colorId" value="darkblue">  Dark Blue',
+    '<input type="radio" name ="colorId" value="red">  Red'
 );
 
-var colorPhaseFour = new Phase (
+var colorPhaseFour = new Phase(
     'What do you think? If you\'re happy with it, submit it to see your changes, so far.',
     '<input type="submit" value="Onward!"'
 );
 
-Phase.prototype.executePhase = function(){
-        colorNarrator.talkLoc.innerText = this.talkAt;
-        colorNarrator.buttonOne.innerHTML = this.buttonOne;
+Phase.prototype.executePhase = function () {
+	colorNarrator.talkLoc.innerText = this.talkAt;
+	colorNarrator.buttonOne.innerHTML = this.buttonOne;
 
-if (this.inputOne) {
-    colorNarrator.listenLocOne.innerHTML = this.inputOne;
-}
+	if (this.inputOne) {
+		colorNarrator.listenLocOne.innerHTML = this.inputOne;
+	}
 
-if (this.inputTwo) {
-    colorNarrator.listenLocTwo.innerHTML = this.inputTwo;
-}
+	if (this.inputTwo) {
+		colorNarrator.listenLocTwo.innerHTML = this.inputTwo;
+	}
 
-if (this.buttonTwo) {
-    colorNarrator.buttonTwo.innerHTML = this.buttonTwo;
-}
-    };
+	if (this.buttonTwo) {
+		colorNarrator.buttonTwo.innerHTML = this.buttonTwo;
+	}
+};
 
-colorNarrator.saveGuest = function() {
-    this.guest = colorUser;
+colorNarrator.saveGuest = function () {
+	this.guest = colorUser;
 };
 
 //  give the user a page to view
 
-var onloadPage = function() {
-    colorPhaseOne.executePhase();
-// colorNarrator.talkLoc.innerText = colorPhaseOne.talkAt;
-// colorNarrator.buttonOne.innerHTML = colorPhaseOne.buttonOne;
-
-// if (colorPhaseOne.inputOne) {
-//     colorNarrator.listenLocOne.innerHTML = colorPhaseOne.inputOne;
-// }
-
-// if (colorPhaseOne.inputTwo) {
-//     colorNarrator.listenLocTwo.innerHTML = colorPhaseOne.inputTwo;
-// }
-
-// if (colorPhaseOne.buttonTwo) {
-//     colorNarrator.buttonTwo.innerHTML = colorPhaseOne.buttonTwo;
-// }
-
-};
-
-onloadPage();
+colorPhaseOne.executePhase();
 
 var submitButton = document.getElementById('buttonOne');
 submitButton.addEventListener('click', submitHandler);
 
 function submitHandler() {
-    event.preventDefault();
-    // colorUser.userSubmission();
-    colorNarrator.saveGuest();
-    colorNarrator.locallyStoreUser();
-    window.location.href= './roughdraft.html';
+	event.preventDefault();
+	if (currentPhase === 1) {
+		var colorIds = document.getElementsByName('colorId');
+		var selectedColor = [];
+		for (var i = 0; i < colorIds.length; i++) {
+			if (colorIds[i].checked) {
+				selectedColor.push(colorIds[i].value);
+			}
+		}
+		colorUser.color1 = selectedColor;
+        document.getElementsByTagName('body')[0].setAttribute('class',selectedColor);
+		colorPhaseTwo.executePhase();
+		currentPhase += 1;
+	}
+
+	colorPhaseThree.executePhase();
+	colorPhaseFour.executePhase();
+
+    //save to localstorage and move to next page
+	colorNarrator.saveGuest();
+	colorNarrator.locallyStoreUser();
+	window.location.href = './roughdraft.html';
 }
 
 
