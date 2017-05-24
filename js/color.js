8ec8aa7581b57f6477b8781d1d722ccca810b05c
Joe Churchman <churchman.joe@gmail.com>
2017-05-24 11:59:50 -0700

    established colorPhase objects, modified narrator.js Loc names

--- js/color.js
+++ js/color.js
@@ -18,31 +18,34 @@ function submitHandler() {
     window.location.href= './roughdraft.html';
 }
 
-colorNarrator.talkAt = [
+var colorPhaseOne = new Phase (
     'Welcome to a world of color! Black and white can be very appealing, but color can also add a lot to a page. Let\'s experiment! First, choose a color for the background of your page.',
-    'Next, choose a color for your header.',
-    'Lastly, choose a color for the text in your paragraph.',
-    'What do you think? If you\'re happy with it, submit it to see your changes, so far.'
-];
-colorNarrator.listenTo = [
+    '<input type="submit" value="Onward!"',
     '<input type="radio" value="grey">', 
+    '<input type="radio" value="lightblue">' 
+);
+
+var colorPhaseTwo = new Phase (
+    'Next, choose a color for your header.',
+    '<input type="submit" value="Onward!"',
     '<input type="radio" value="darkgreen">', 
+    '<input type="radio" value="maroon">' 
+);
+
+var colorPhaseThree = new Phase (
+    'Lastly, choose a color for the text in your paragraph.',
+    '<input type="submit" value="Onward!"',
     '<input type="radio" value="darkblue">', 
-    '',
-    '<input type="radio" value="lightblue">', 
-    '<input type="radio" value="maroon">', 
-    '<input type="radio" value="red">',
-    ''
-];
-colorNarrator.moveOn = [
-    '<input type="submit" value="Onward!"',
-    '<input type="submit" value="Onward!"',
-    '<input type="submit" value="Onward!"',
-    '<input type="submit" value="Onward!"',
-];
+    '<input type="radio" value="red">'
+);
+
+var colorPhaseFour = new Phase (
+    'What do you think? If you\'re happy with it, submit it to see your changes, so far.',
+    '<input type="submit" value="Onward!"'
+);
 
 
 
 
-var inputSpotOne = document.getElementById('inputOne');
-inputSpotOne.innerHTML = '<input type="radio" value="grey">';
+// var inputSpotOne = document.getElementById('inputOne');
+// inputSpotOne.innerHTML = '<input type="radio" value="grey">';
