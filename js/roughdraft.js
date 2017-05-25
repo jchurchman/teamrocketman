c7f52d36621ed2f6dca60bb2a26a1ee8e8f0b325
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-05-25 13:44:40 -0700

    add rough draft phases

--- js/roughdraft.js
+++ js/roughdraft.js
@@ -27,3 +27,19 @@ function submitHandler() {
     roughDraftNarrator.locallyStoreUser();
     window.location.href= './final.html';
 }
+
+var roughDraftPhaseOne = new Phase (
+    'This is pretty much what your page looks like after incorporating the choices that you\'ve made. We want to show you the final page, but we\'ve lost the link to the final submit button! Click below to find it.',
+    '<input type="submit" value="submit" id="actualButtonOne">',
+    null,
+    null,
+    null
+);
+
+var roughDraftPhaseTwo = new Phase (
+    'In order to find the button, we need your help selecting an element from the drop-down menu.',
+    '<input type"submit" value="This one?" id="actualButtonOne">',
+    '<select class="inputBox" id="inputFieldOne" name="Tag List"><option value="img">Image</option><option value="p">Paragraph</option><option value="h1">Header</option><option value="ul">Nav List</option>',
+    null,
+    null
+);
