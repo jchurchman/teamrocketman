ae82240b0c72b1b3ad5315ce97164698020e2bbd
Joe Churchman <churchman.joe@gmail.com>
2017-05-25 14:39:49 -0700

    work on roughdraft.js

--- js/roughdraft.js
+++ js/roughdraft.js
@@ -2,7 +2,7 @@
 
 var roughDraftNarrator = new Narrator ();
 var roughDraftUser = new User();
-// var currentPhase = 1;
+var currentPhase = 1;
 
 roughDraftNarrator.getGuest();
 roughDraftNarrator.popContent();
@@ -13,24 +13,9 @@ roughDraftNarrator.saveGuest = function() {
     this.guest = roughDraftUser;
 };
 
-roughDraftNarrator.popPageUser = function () {
-    roughDraftUser = this.guest;
-};
-
-var submitButton = document.getElementById('buttonDiv');
-submitButton.addEventListener('click', submitHandler);
-
-function submitHandler() {
-    event.preventDefault();
-    roughDraftUser.userSubmission();
-    roughDraftNarrator.saveGuest();
-    roughDraftNarrator.locallyStoreUser();
-    window.location.href= './final.html';
-}
-
 var roughDraftPhaseOne = new Phase (
     'This is pretty much what your page looks like after incorporating the choices that you\'ve made. We want to show you the final page, but we\'ve lost the link to the final submit button! Click below to find it.',
-    '<input type="submit" value="submit" id="actualButtonOne">',
+    '<input type="submit" value="Let\'s do it!" id="actualButtonOne">',
     null,
     null,
     null
@@ -38,8 +23,49 @@ var roughDraftPhaseOne = new Phase (
 
 var roughDraftPhaseTwo = new Phase (
     'In order to find the button, we need your help selecting an element from the drop-down menu.',
-    '<input type"submit" value="This one?" id="actualButtonOne">',
+    '<input type="submit" value="This one?" id="actualButtonOne">',
     '<select class="inputBox" id="inputFieldOne" name="Tag List"><option value="img">Image</option><option value="p">Paragraph</option><option value="h1">Header</option><option value="ul">Nav List</option>',
     null,
     null
 );
+
+roughDraftNarrator.popPageUser = function () {
+    roughDraftUser = this.guest;
+};
+
+Phase.prototype.executePhase = function () {
+    roughDraftNarrator.talkLoc.innerHTML = this.talkAt;
+    roughDraftNarrator.buttonOne.innerHTML = this.buttonOne;
+    if (this.inputOne) {
+        roughDraftNarrator.listenLocOne.innerHTML = this.inputOne;
+    }
+    if (this.inputTwo) {
+        roughDraftNarrator.listenLocTwo.innerHTML = this.inputTwo;
+    }
+    if (this.buttonTwo) {
+        roughDraftNarrator.buttonTwo.innerHTML = this.buttonTwo;
+    }
+};
+
+roughDraftPhaseOne.executePhase();
+
+var submitButton = document.getElementById('buttonDiv');
+submitButton.addEventListener('click', submitHandler);
+
+function submitHandler() {
+    event.preventDefault();
+    if (currentPhase === 1) {
+        roughDraftNarrator.clearThroat();
+        roughDraftPhaseTwo.executePhase();
+        currentPhase += 1;
+    } else if (currentPhase === 2) {
+    roughDraftNarrator.saveGuest();
+    roughDraftNarrator.locallyStoreUser();
+
+    }
+
+
+
+
+    // window.location.href= './final.html';
+}
