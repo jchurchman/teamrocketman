6e927ac7e96805f1514545c41689e6df704007bb
Joe Churchman <churchman.joe@gmail.com>
2017-05-25 15:07:05 -0700

    roughed in roughdraft narrator

--- js/roughdraft.js
+++ js/roughdraft.js
@@ -9,10 +9,6 @@ roughDraftNarrator.popContent();
 roughDraftNarrator.popStyles();
 roughDraftNarrator.popColors();
 
-roughDraftNarrator.saveGuest = function() {
-    this.guest = roughDraftUser;
-};
-
 var roughDraftPhaseOne = new Phase (
     'This is pretty much what your page looks like after incorporating the choices that you\'ve made. We want to show you the final page, but we\'ve lost the link to the final submit button! Click below to find it.',
     '<input type="submit" value="Let\'s do it!" id="actualButtonOne">',
@@ -24,7 +20,7 @@ var roughDraftPhaseOne = new Phase (
 var roughDraftPhaseTwo = new Phase (
     'In order to find the button, we need your help selecting an element from the drop-down menu.',
     '<input type="submit" value="This one?" id="actualButtonOne">',
-    '<select class="inputBox" id="inputFieldOne" name="Tag List"><option value="img">Image</option><option value="p">Paragraph</option><option value="h1">Header</option><option value="ul">Nav List</option>',
+    '<select class="inputBox" id="inputFieldOne" name="Tag List"><option value="img">Image</option><option value="h1">Header</option><option value="h2">SubHeader</option><option value="h3">Article Header</option><option value="ul">Nav List</option>',
     null,
     null
 );
@@ -32,6 +28,8 @@ var roughDraftPhaseTwo = new Phase (
 roughDraftNarrator.popPageUser = function () {
     roughDraftUser = this.guest;
 };
+
+roughDraftNarrator.popPageUser();
 
 Phase.prototype.executePhase = function () {
     roughDraftNarrator.talkLoc.innerHTML = this.talkAt;
@@ -59,13 +57,27 @@ function submitHandler() {
         roughDraftPhaseTwo.executePhase();
         currentPhase += 1;
     } else if (currentPhase === 2) {
-    roughDraftNarrator.saveGuest();
-    roughDraftNarrator.locallyStoreUser();
-
+        switch (document.querySelector('#inputFieldOne option:checked').value) {
+            case ('h1'):
+                document.getElementsByTagName('header')[0].removeChild(document.getElementsByTagName('h1')[0]);
+                break;
+            case ('ul'):
+                document.getElementsByTagName('header')[0].removeChild(document.getElementsByTagName('ul')[0]);
+                break;
+            case ('h2'):
+                document.getElementsByTagName('header')[0].removeChild(document.getElementsByTagName('h2')[0]);
+                break;
+            case ('img'):
+                document.getElementsByTagName('p')[0].removeChild(document.getElementsByTagName('img')[0]);
+                break;
+            case ('h3'):
+                var articleHeaderLoc = document.getElementsByTagName('h3')[0];
+                articleHeaderLoc.innerHTML = '<input type="submit" value="I\'m done!" id="actualButtonTwo">';
+                var moveOnButton = document.getElementById('actualButtonTwo');
+                moveOnButton.addEventListener('click', moveOnHandler);
+        }
     }
-
-
-
-
-    // window.location.href= './final.html';
 }
+    function moveOnHandler() {
+        window.location.href = './final.html';
+    }
